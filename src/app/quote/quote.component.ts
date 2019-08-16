a01eb1a9c81ed94ccf7c580b9e4d54c198b9ed54
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-16 16:32:03 +0300

    add quote array.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  myQuotes:Quote[] = [
+    {id:1, title:'Life Quotes', quote:'All that we are is the result of what we have thought'},
+    {id:2, title:'Health Quotes', quote:'A few germs never hurt anyone'},
+    {id:3, title:'Wisdom Quotes',quote: 'A great man is always willing to be little'},
+  ];
 
   constructor() { }
 
