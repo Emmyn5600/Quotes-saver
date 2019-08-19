8ee44f64e0cad977b0fd8092e69d155b9fcfadca
Christine-N-Mwaura <njerimwaura25@gmail.com>
2019-08-19 09:42:20 +0300

    add changes to ts files and pipes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,13 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   myQuotes:Quote[] = [
-    new Quote(1, 'Life Quotes', 'All that we are is the result of what we have thought','Serena Williams',new Date()),
-    new Quote(2, 'Health Quotes', 'A few germs never hurt anyone','Serena Williams',new Date()),
-    new Quote(3, 'Wisdom Quotes', 'A great man is always willing to be little','Serena Williams',new Date()),
+    new Quote(1, 'Life Quote', 'All that we are is the result of what we have thought','Serena Williams',new Date(2019,9,14)),
+    new Quote(2, 'Health Quote', 'A few germs never hurt anyone','Serena Williams',new Date(2019,6,9)),
+    new Quote(3, 'Wisdom Quote','Sometimes the questions are complicated, and the answers are simple.','Dr. Seuss',new Date(2019,1,12)),
+    // new Quote(4, 'Life Quote', 'Life can only be understood backwards; but it must be lived forwards.','Søren Kierkegaard',new Date(2019,11,18)),
+    // new Quote(5, 'Wisdom Quote', 'There are only two great tragedies in life: one is not getting what you want and the other is getting it.','Oscar Wilde',new Date(2019,2,14)),
+    // new Quote(6, 'Wisdom Quote', 'Destiny is a name often given in retrospect to choices that had dramatic consequences.','J.K. Rowling',new Date(2019,3,14)),
+
   ];
 showAuthor(index){
   this.myQuotes[index].moreDetails = !this.myQuotes[index].moreDetails;
